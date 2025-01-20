3ae220cc91883557d53fbc4db76eb05e10eede8a
photoby64 <photoby64@naver.com>
2025-01-20 14:46:37 +0900

    learn.react
    - 25.01.20 수업 내용(f)
    - Chip & ChipList 컴포넌트 작성
    - 디자인 → 데이터 분리
    - 코드 → 데이터 추출 관리

--- src/playground.tsx
+++ src/playground.tsx
@@ -1,39 +1,25 @@
 import { useState } from 'react';
 import type { ChipListType } from '@/types/chip';
 import ChipList from '@/components/chip-list';
+import { chips } from '@/datas/chips';
 
 function Playground() {
-  const [chipData] = useState<ChipListType>([
-    {
-      id: 1,
-      label: '전체',
-    },
-    {
-      id: 2,
-      label: '아우터',
-    },
-    {
-      id: 3,
-      label: '바지',
-    },
-    {
-      id: 4,
-      label: '티셔츠',
-    },
-    {
-      id: 5,
-      label: '점퍼',
-    },
-    {
-      id: 6,
-      label: '원피스',
-    },
-  ]);
+  const [chipData] = useState<ChipListType>(chips);
 
   return (
-    <div className="Playground">
+    <div className="Playground" style={{ marginInline: 40 }}>
       <h1>Playground</h1>
-      <ChipList items={chipData} />
+      <div
+        style={{
+          inlineSize: 320,
+          blockSize: 640,
+          border: '4px solid rgba(0 0 0 / 10%)',
+          borderRadius: 12,
+          padding: 8,
+        }}
+      >
+        <ChipList items={chipData} />
+      </div>
     </div>
   );
 }
