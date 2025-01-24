694319c9864b19e2c8c64107b832aa0b751057a3
photoby64 <photoby64@naver.com>
2025-01-24 10:31:22 +0900

    learn.react
    - 25.01.24 오전 수업 Form 외부에 있는 컨트롤을 formData로 연결
    - form 속성 사용
    - <form> 요소의 id에 연결

--- src/playgrounds/02-react-form.tsx
+++ src/playgrounds/02-react-form.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import FormInput from '@/components/form-input';
 import FormTextArea from '@/components/form-textarea';
 
+// React에 의해 제어되는 폼 입력 초깃값
 const initialFormData = {
   limitAge: 40,
   profileImage: null,
+  color: '#ffffff',
   photos: [],
   hobbies: [
     {
@@ -35,13 +37,18 @@ const initialFormData = {
 };
 
 function ReactForm() {
-  // 현재 관리되는 폼 데이터 상태: limitAge, profileImage, photos, hobbyList
-
+  // React에 의해 제어되는 입력 값 초기화 함수
   const handleResetForm = () => {
     setLimitAge(initialFormData.limitAge);
     setProfileImage(initialFormData.profileImage);
     setPhotos(initialFormData.photos);
     setHobbyList(initialFormData.hobbies);
+  };
+
+  const [color, setColor] = useState<string>(initialFormData.color);
+  const handleChangeColor = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const nextColorValue = e.currentTarget.value;
+    setColor(nextColorValue);
   };
 
   const [limitAge, setLimitAge] = useState<number>(initialFormData.limitAge);
@@ -110,7 +117,16 @@ function ReactForm() {
     <div className="ReactForm">
       <h2>React 폼(form)</h2>
 
-      <form style={formStyles}>
+      <div className="ComboBox">
+        <label htmlFor="spice-pick">향신료 선택</label>
+        <select form="ReactForm" id="spice-pick" name="spice-pick">
+          <option value="lemongrass">레몬그라스</option>
+          <option value="rosmari">로즈마리</option>
+          <option value="lavender">라벤더</option>
+        </select>
+      </div>
+
+      <form id="ReactForm" style={formStyles}>
         {/* type=radio */}
         <fieldset>
           <legend>성별</legend>
@@ -170,7 +186,17 @@ function ReactForm() {
         <FormInput type="number" label="나이" defaultValue={24} />
 
         {/* type=color */}
-        <FormInput type="color" label="색상" defaultValue="#ffffff" />
+        <div style={{ display: 'flex', gap: 8, alignItems: 'center' }}>
+          <FormInput
+            type="color"
+            label="색상"
+            value={color}
+            onChange={handleChangeColor}
+          />
+          <output style={{ translate: '0 12px', color, fontWeight: 700 }}>
+            {color}
+          </output>
+        </div>
 
         {/* type=range */}
         <div style={{ display: 'flex', gap: 8, alignItems: 'center' }}>
